13fcc33af97fcd87ec4cea29b5b9d168e33c8b46
daegyujeong <61696336+daegyujeong@users.noreply.github.com>
2020-05-03 20:04:04 +0900

    1
    1

--- weather.js
+++ weather.js
@@ -13,7 +13,7 @@ function paintImage(iconId) {
 
 function getWeather(lat, lon) {
   fetch(
-    `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
   )
     .then(function (response) {
       // API를 불러오면..
